80ca41e66fc5ccd63b409450ef0eb9944598651a
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-02 15:14:26 +0300

    update

--- dict/dict.view.ts
+++ dict/dict.view.ts
@@ -1,29 +1,27 @@
 namespace $.$$ {
 	export class $invite_dict extends $.$invite_dict {
-		@ $mol_mem
+		@$mol_mem
 		word_label( next?: any ): string {
 			return this.word() || 'HUMMINGBIRD'
 		}
 
-		@ $mol_mem
+		@$mol_mem
 		word( next?: any ): string {
-			return this.$.$mol_state_arg.value('word', next) ?? ''
+			return this.$.$mol_state_arg.value( 'word', next ) ?? ''
 		}
 
-
-		@ $mol_mem
+		@$mol_mem
 		transcription( next?: any ): string {
-			if (this.word() === '') return 'hʌ́mɪŋbəːd'
-			if (this.word()) {
+			if( this.word() === '' ) return 'hʌ́mɪŋbəːd'
+			if( this.word() ) {
 				try {
-					return this.$.$mol_fetch.text(this.api_uri() + this.word())
-				} catch (error) {
+					return this.$.$mol_fetch.text( this.api_uri() + this.word() )
+				} catch( error ) {
 					if( error instanceof Promise ) $mol_fail_hidden( error )
 					return 'Ошибка запроса'
 				}
 			}
 			return next ?? ''
 		}
-
 	}
 }
